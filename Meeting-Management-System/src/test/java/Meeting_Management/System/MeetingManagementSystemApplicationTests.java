d6ceb3f90d1e1c95663304b7806db10207b1955e
@mainali10 <sandeep.mn10@gmail.com>
2025-03-24 12:01:59 +0545

    project setup

---
+++ Meeting-Management-System/src/test/java/Meeting_Management/System/MeetingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Meeting_Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
