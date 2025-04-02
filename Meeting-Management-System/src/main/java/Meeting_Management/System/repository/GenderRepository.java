eeb6f79ce8e0a62d3f8c99b13c125f3c8a1d9618
Grish Shrestha <grishshrestha2@gmail.com>
2025-04-02 11:04:28 +0545

    Deleted unused entity and repository files

--- Meeting-Management-System/src/main/java/Meeting_Management/System/repository/GenderRepository.java
+++
@@ -1,9 +0,0 @@
-package Meeting_Management.System.repository;
-
-import Meeting_Management.System.entity.Gender;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenderRepository extends JpaRepository<Gender, Integer> {
-}
